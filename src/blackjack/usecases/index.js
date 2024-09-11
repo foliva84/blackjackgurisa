ba69d3e549da737d0206dad9f648e8023cedb306
Fernando Oliva <fernandogermanoliva@gmail.com>
2024-09-11 00:29:34 -0300

    First Commit

---
+++ src/blackjack/usecases/index.js
@@ -0,0 +1,7 @@
+export { crearCartaHTML  } from './crear-carta-html';
+export { crearDeck } from './crear-deck';
+export { pedirCarta } from './pedir-carta';
+export { turnoComputadora } from './turno-computadora';
+export { valorCarta } from './valor-carta';
+
+// Archivo barril según Fernando Herrera
