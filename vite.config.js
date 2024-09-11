5657aa681d385847efeecfd1a2e22de2c16faa51
Fernando Oliva <fernandogermanoliva@gmail.com>
2024-09-11 01:03:15 -0300

    dist updated 5

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    base: './',  // Usa './' para generar rutas relativas
+    base: '/blackjackgurisa/',
+    plugins: [],
 });
